db38d6cd9ebd6c0ff15fa7543a4512f4e49d60d8
kuchukpie@gmail.com <kuchukboromd@gmail.com>
2022-12-26 23:31:05 +0530

    Add project files.

---
+++ KukusVillagerMod/KukusVillagerMod.cs
@@ -0,0 +1,49 @@
+// KukusVillagerMod
+// a Valheim mod skeleton using Jötunn
+// 
+// File:    KukusVillagerMod.cs
+// Project: KukusVillagerMod
+
+using BepInEx;
+using Jotunn.Entities;
+using Jotunn.Managers;
+
+namespace KukusVillagerMod
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency(Jotunn.Main.ModGuid)]
+    //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
+    internal class KukusVillagerMod : BaseUnityPlugin
+    {
+        public const string PluginGUID = "com.jotunn.KukusVillagerMod";
+        public const string PluginName = "KukusVillagerMod";
+        public const string PluginVersion = "1.0.0";
+
+        // Use this class to add your own localization to the game
+        // https://valheim-modding.github.io/Jotunn/tutorials/localization.html
+        public static CustomLocalization Localization = LocalizationManager.Instance.GetLocalization();
+
+        private void Awake()
+        {
+            // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
+            Jotunn.Logger.LogInfo("KukusVillagerMod has landed");
+
+            // To learn more about Jotunn's features, go to
+            // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
+        }
+    }
+
+
+    class KLog
+    {
+        public static void info(string msg)
+        {
+            Jotunn.Logger.LogInfo(msg);
+        }
+        public static void warning(string msg)
+        {
+            Jotunn.Logger.LogWarning(msg);
+        }
+    }
+}
+
---
+++ KukusVillagerMod/Prefabs/BedPrefab.cs
@@ -0,0 +1,58 @@
+﻿using Jotunn.Configs;
+using Jotunn.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KukusVillagerMod.Prefabs
+{
+    class BedPrefab
+    {
+        private void createBed(string bedID, string bedDesc, string cloneName, string villagerCreatureName, List<RequirementConfig> requirements, int villagerLevel, float villagerHealth, int villagerType)
+        {
+            //Create Configuration of the bed
+            PieceConfig bedConfig = new PieceConfig();
+            bedConfig.Name = bedID.Replace("_", " ");
+            bedConfig.PieceTable = "Hammer";
+            bedConfig.Category = "Villager";
+
+            //Update requirements
+            foreach (var r in requirements)
+            {
+                bedConfig.AddRequirement(r);
+            }
+
+
+            bedConfig.Description = bedDesc;
+
+
+            //Create the Bed Piece (Custom Piece)
+            var bed = new CustomPiece(bedID, cloneName, bedConfig);
+
+            //Remove default interactions of the bed
+            UnityEngine.Object.DestroyImmediate(bed.PiecePrefab.GetComponent(typeof(Interactable)));
+            UnityEngine.Object.DestroyImmediate(bed.PiecePrefab.GetComponent(typeof(Hoverable)));
+            UnityEngine.Object.DestroyImmediate(bed.PiecePrefab.GetComponent(typeof(Bed)));
+
+
+            //Add Spawner component and update spawner creature name
+            var spawner = bed.PiecePrefab.GetOrAddComponent<BedState>();
+            spawner.villagerName = villagerCreatureName;
+            spawner.villagerLevel = villagerLevel;
+            spawner.villagerType = villagerType;
+            spawner.villagerHealth = villagerHealth;
+
+            //Add a container. Will use in future versions maybe
+            bed.PiecePrefab.AddComponent<Container>();
+
+            //Add the piece to PieceManager
+            PieceManager.Instance.AddPiece(bed);
+
+            KLog.logInfo($"Created : {bedID} with villager : {spawner.villagerName}, level : {spawner.villagerLevel}");
+        }
+    }
+
+
+}
