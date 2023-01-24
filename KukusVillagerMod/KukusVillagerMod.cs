fa00140a802d6e0e62f1601e52529d5735cda7a5
kuchukpie@gmail.com <kuchukboromd@gmail.com>
2023-01-24 06:17:08 +0530

    added another solution

--- KukusVillagerMod/KukusVillagerMod.cs
+++ KukusVillagerMod/KukusVillagerMod.cs
@@ -940,7 +940,7 @@ namespace KukusVillagerMod
  * Recruit Villagers using Gold
  * Villagers help with fueling
  * Potion to heal villagers
- * Find better way to move villagers. they try to go directly to path (Dynamically decreasing await time only when staring at player or not moving, trying to use moveTo, moveAvoid, follow etc)
+ * Find better way to move villagers. they try to go directly to path (Dynamically decreasing await time only when staring at player or not moving, trying to use moveTo, moveAvoid, follow etc. or see follow me and try to write your own version of it)
  * Fixing base
  * Mining rock
  */
